6d5e137745b7eacdea97959e80c6b3102104814c
Viktoriya Khvin <106774581+ViktoriyaKhvin@users.noreply.github.com>
2022-08-08 17:52:04 +0300

    Delete custom-checker.ts

--- hw-4/custom-checker.ts
+++
@@ -1,9 +0,0 @@
-import { calculator } from "./calculator";
-
-export function customChecker (firstNumber: number, secondNumber: number, operator:'+' | '-' | '*' | '/' , result: number) {
- if (calculator (firstNumber, secondNumber, operator ) === result) {
-console.log ('Test passed');
- } else {
-console.log ('Test failed');
- }
- }
