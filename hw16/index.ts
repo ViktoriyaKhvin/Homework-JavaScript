a1bb1523e64730744a79a3bf90b40916fe84a901
ViktoriyaKhvin <khvinka@yandex.by>
2022-08-12 13:32:25 +0300

    hw16

--- hw16/index.ts
+++ hw16/index.ts
@@ -7,7 +7,7 @@ import { customChecker } from "./custom-checker";
  customChecker(10, 0, '-', -10);
  customChecker(-2, 9, '+', 7);
  customChecker(1, 1, '*', 1);
- customChecker(90, 10, '/', 9);
+ customChecker(80, 10, '/', 8);
 
 
  //negative checks
