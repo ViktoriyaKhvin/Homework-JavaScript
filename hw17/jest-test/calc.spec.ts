95a1d479362344fa6912f24dca526c999abf2632
ViktoriyaKhvin <khvinka@yandex.by>
2022-08-12 20:55:30 +0300

    hw17

--- hw17/jest-test/calc.spec.ts
+++ hw17/jest-test/calc.spec.ts
@@ -3,28 +3,28 @@ import { expect } from "chai";
  const calc = new Calculator();
 
  describe("Jest Calculator Test", () => {
-   test("Correctly finds the sum of two numbers", () => {
+   test("Correctly finds the sum of two positive numbers", () => {
      expect(calc.addition(1, 3)).to.equal(4);
    });
-   test("Correctly finds the difference of two numbers", () => {
+   test("Correctly finds the difference of two positive numbers", () => {
      expect(calc.subtraction(33, 3)).to.equal(30);
    });
-   test("Correctly finds the product of two numbers", () => {
+   test("Correctly finds the  multiplication of two positive numbers", () => {
      expect(calc.multiplication(12, 12)).to.equal(144);
    });
-   test("Correctly finds the quotient of two numbers", () => {
+   test("Correctly finds the quotient of two positive numbers", () => {
      expect(calc.division(10, 2)).to.equal(5);
    });
-   test("Correctly finds the sum of two numbers", () => {
+   test("Correctly finds the sum of two negative numbers", () => {
      expect(calc.addition(-1, -1)).to.equal(-2);
    });
-   test("Correctly finds the difference of two numbers", () => {
+   test("Correctly finds the difference of positive and negative numbers", () => {
      expect(calc.subtraction(-5, 0)).to.equal(-5);
    });
-   test("Correctly finds the product of two numbers", () => {
+   test("Correctly finds the  multiplication of positive and negative numbers", () => {
      expect(calc.multiplication(-10, 10)).to.equal(-100);
    });
-   test("Correctly finds the quotient of two numbers", () => {
+   test("Correctly finds the quotient of positive and negative numbers", () => {
      expect(calc.division(99, -9)).to.equal(-11);
    });
  });
--- hw17/mocha-test/calc.spec.ts
+++ hw17/mocha-test/calc.spec.ts
@@ -4,28 +4,28 @@ import { Calculator } from "../src/calculator";
 
  describe("Mocha Calculator Test", () => {
 
-   it("Correctly finds the sum of two numbers", () => {
+   it("Correctly finds the sum of two positive numbers", () => {
      assert.equal(calc.addition(1, 3), 4);
    });
-   it("Correctly finds the difference of two numbers", () => {
+   it("Correctly finds the difference of two positive numbers", () => {
      assert.equal(calc.subtraction(33, 3), 30);
    });
-   it("Correctly finds the product of two numbers", () => {
+   it("Correctly finds the multiplication of two positive numbers", () => {
      assert.equal(calc.multiplication(12, 12), 144);
    });
-   it("Correctly finds the quotient of two numbers", () => {
+   it("Correctly finds the quotient of two positive numbers", () => {
      assert.equal(calc.division(10, 2), 5);
    });
-   it("Correctly finds the sum of two numbers", () => {
+   it("Correctly finds the sum of two negative numbers", () => {
      assert.equal(calc.addition(-1, -1), -2);
    });
-   it("Correctly finds the difference of two numbers", () => {
+   it("Correctly finds the difference of positive and negative numbers", () => {
      assert.equal(calc.subtraction(-5, 0), -5);
    });
-   it("Correctly finds the product of two numbers", () => {
+   it("Correctly finds the multiplication of positive and negative numbers", () => {
      assert.equal(calc.multiplication(-10, 10), -100);
    });
-   it("Correctly finds the quotient of two numbers", () => {
+   it("Correctly finds the quotient of positive and negative numbers", () => {
      assert.equal(calc.division(99, -9), -11);
    });
  });
