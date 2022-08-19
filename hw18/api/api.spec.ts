3ca4282ce60f8d4a80e6c1da6efa3cab20cb0d1e
ViktoriyaKhvin <khvinka@yandex.by>
2022-08-19 23:06:44 +0300

    hw18

--- hw18/api/api.spec.ts
+++ hw18/api/api.spec.ts
@@ -1,138 +1,132 @@
-import superagent from "superagent";
+ import superagent from "superagent";
+ import { describe, expect } from "@jest/globals";
 
- describe('API tests:', () => {
-     let response: any;
-     let formData: any;
+ describe("Test HTTP methods", () => {
+   const baseURL = "https://jsonplaceholder.typicode.com";
 
-     describe('GET requests:', () => {
-         it('Should correctly get data of users list', async () => {
-             try {
-                 response = await superagent.get("https://reqres.in/api/users");
-             } catch (e: any) {
-                 console.log(e.message);
-             }
+   test("Should return 200 HTTP status code for GET response", () => {
+     return superagent
+       .get(baseURL + "/posts/6")
+       .then((response) => {
+         expect(response.status).toBe(200);
+       })
+       .catch((error) => {
+         throw new Error(error.message);
+       });
+   });
 
-             expect(response.status).toBe(200);
-             expect(response.body.data.length).toBeGreaterThan(0);
-         });
 
-         it('Should correctly get data of list resource', async () => {
-             try {
-                 response = await superagent.get("https://reqres.in/api/unknown")
-             } catch (e: any) {
-                 console.log(e.message);
-             }
+   test("Should successfully create a post", () => {
+     const userId = 1337;
+     const postTitle = "postTitle";
+     const postBody = "postBody";
 
-             expect(response.status).toBe(200);
-             expect(response.body.data.length).toBeGreaterThan(0);
-         });
-     });
+     return superagent
+       .post(baseURL + "/posts")
+       .send({ userId: userId, title: postTitle, body: postBody })
+       .then((response) => {
+         expect(response.status).toBe(201);
+         expect(response.body.userId).toBe(userId);
+         expect(response.body.title).toBe(postTitle);
+         expect(response.body.body).toBe(postBody);
+         expect(response.body.id).toBe(101);
+       })
+       .catch((error) => {
+         throw new Error(error.message);
+       });
+   });
 
-     describe('POST requests:', () => {
-         it('Should correctly create new user', async () => {
-             formData = {
-                 name: "Viktoriya",
-                 job: "Manager"
-             }
+   test("Should successfully update first post's title via PATCH method", () => {
+     const updatedTitle = "postTitle";
 
-             try {
-                 response = await superagent.post("https://reqres.in/api/users")
-                     .set('Content-Type', 'Application/json')
-                     .send(formData);
-             } catch (e: any) {
-                 console.log(e.message);
-             }
+     return superagent
+       .patch(baseURL + "/posts/1")
+       .send({ title: updatedTitle })
+       .then((response) => {
+         expect(response.status).toBe(200);
+         expect(response.body.id).toBe(1);
+         expect(response.body.title).toBe(updatedTitle);
+         expect(response.body.userId).not.toBe(null);
+         expect(response.body.body).not.toBe(null);
+       })
+       .catch((error) => {
+         throw new Error(error.message);
+       });
+   });
 
-             expect(response.status).toBe(201);
-             expect(response.body.hasOwnProperty(formData));
-         });
+   test("Should successfully update post's title via PUT method", () => {
+     const updatedTitle = "postTitle";
 
-         it('Should correctly get auth token', async () => {
-             formData = {
-                 email: "barsik@yandex.by",
-                 password: "barsik2022"
-             }
+     return superagent
+       .put(baseURL + "/posts/1")
+       .send({ title: updatedTitle })
+       .then((response) => {
+         expect(response.status).toBe(200);
+         expect(response.body.id).toBe(1);
+         expect(response.body.title).toBe(updatedTitle);
+         expect(response.body.userId).toBeUndefined();
+         expect(response.body.body).toBeUndefined();
+       })
+       .catch((error) => {
+         throw new Error(error.message);
+       });
+   });
 
-             try {
-                 response = await superagent.post("https://reqres.in/api//login")
-                     .set('Content-Type', 'Application/json')
-                     .send(formData);
-             } catch (e: any) {
-                 console.log(e.message);
-             }
+   test("Should successfully delete post", () => {
+     return superagent
+       .delete(baseURL + "/posts/1")
+       .then((response) => {
+         expect(response.status).toBe(200);
+         expect(response.body).toEqual({});
+       })
+       .catch((error) => {
+         throw new Error(error.message);
+       });
+   });
 
-             expect(response.status).toBe(200);
-             expect(response.body.hasOwnProperty('token'));
-             expect(response.body.token.length).toBeGreaterThan(0);
-         });
+   test("Should return 404 HTTP Status code if the post doesn't exist", () => {
+     return superagent
+       .get(baseURL + "/posts/101")
+       .then()
+       .catch((error) => {
+         expect(error.status).toBe(404);
+       });
+   });
 
-         it('Should get error message after auth request with uncorrectly credentials', async () => {
-             formData = {
-                 email: "lol@lol"
-             }
+   test("Should return 500 HTTP Status code for PUT method to the non-existing post", () => {
+     return superagent
+       .put(baseURL + "/posts/101")
+       .send({ id: 101 })
+       .then()
+       .catch((error) => {
+         expect(error.status).toBe(500);
+       });
+   });
 
-             try {
-                 response = await superagent.post("https://reqres.in/api/register")
-                     .set('Content-Type', 'Application/json')
-                     .send(formData);
-             } catch (e: any) {
-                 console.log(e.message);
-             }
+   test("Should return 404 HTTP Status code when DELETE method is used without post id", () => {
+     return superagent
+       .delete(baseURL + "/posts/")
+       .then()
+       .catch((error) => {
+         expect(error.status).toBe(404);
+       });
+   });
 
-             expect(response.status).toBe(400);
-             expect(response.body.error).toBe('Missing password');
-         });
-     });
+   test("Should return 404 HTTP Status code when PATCH method is used without post id and body", () => {
+     return superagent
+       .patch(baseURL + "/posts/")
+       .then()
+       .catch((error) => {
+         expect(error.status).toBe(404);
+       });
+   });
 
-     describe('PUT requests:', () => {
-         it('Should correctly put users data', async () => {
-             formData = {
-                 name: "natasha",
-                 job: "developer"
-             }
-
-             try {
-                 response = await superagent.put("https://reqres.in/api/users/2")
-                     .set('Content-Type', 'Application/json')
-                     .send(formData);
-             } catch (e: any) {
-                 console.log(e.message);
-             }
-
-             expect(response.status).toBe(200);
-             expect(response.body.hasOwnProperty(formData));
-         });
-     });
-
-     describe('PATCH requests:', () => {
-         it('Should correctly patch users data', async () => {
-             formData = {
-                 name: "natasha",
-                 job: "developer"
-             }
-
-             try {
-                 response = await superagent.patch("https://reqres.in/api/users/2")
-                     .set('Content-Type', 'Application/json')
-                     .send(formData);
-             } catch (e: any) {
-                 console.log(e.message);
-             }
-
-             expect(response.status).toBe(200);
-             expect(response.body.hasOwnProperty(formData));
-         });
-     });
-
-     describe('DELETE requests:', () => {
-         it('Should correctly remove the user', async () => {
-             try {
-                 response = await superagent.delete("https://reqres.in/api/users/2")
-             } catch (e: any) {
-                 console.log(e.message);
-             }
-
-             expect(response.status).toBe(204);
-         });
-     });
+   test("Should return 404 HTTP Status code when PUT method is used without post id and body", () => {
+     return superagent
+       .put(baseURL + "/posts/")
+       .then()
+       .catch((error) => {
+         expect(error.status).toBe(404);
+       });
+   });
  });
