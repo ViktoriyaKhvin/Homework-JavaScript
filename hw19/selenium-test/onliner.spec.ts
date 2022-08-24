df389d94c160eaa56637bc3bfff7e3172657757d
ViktoriyaKhvin <khvinka@yandex.by>
2022-08-24 11:13:29 +0300

    hw19

--- hw19/selenium-test/onliner.spec.ts
+++ hw19/selenium-test/onliner.spec.ts
@@ -1,8 +1,29 @@
-import { By, until, Builder, Capabilities, Locator } from "selenium-webdriver"
+import { Builder, Capabilities, By, until, WebElement, } from 'selenium-webdriver';
+import { expect } from 'chai';
 
- const driver = new Builder()
-     .withCapabilities(Capabilities.chrome())
-     .build();
- const baseUrl = "https://www.onliner.by/"
- const defaultWaitingTime = 3000;
+ import { By, Builder, Capabilities, until } from 'selenium-webdriver';
+ import { after, before } from 'mocha';
+ import { expect } from 'chai';
+
+ const baseUrl = 'https://www.onliner.by';
+ const driver = new Builder().withCapabilities(Capabilities.chrome()).build();
+
+ before(async () => {
+    await driver.manage().window().maximize();
+    await driver.get(baseUrl);
+});
+
+it('Should display the title of main page correctly', async () => {
+    await driver.get(baseUrl);
+    const currentTitle = await driver.getTitle();
+    expect(currentTitle).to.equal('Onliner');
+});
+
+it('When User click "Форум", the page is displayed with title including "Форум"', async () => {
+    await driver.findElement(By.linkText('Форум')).click();
+    const catalog = await driver.getTitle();
+    expect(catalog).to.include('Форум');
+});
+  
+
 
