afdb409123d2181839d29422f9484d02f2564588
ViktoriyaKhvin <khvinka@yandex.by>
2022-08-24 23:27:49 +0300

    hw19

--- hw19/selenium-test/onliner.spec.ts
+++ hw19/selenium-test/onliner.spec.ts
@@ -1,51 +1,45 @@
-import { expect } from "chai";
-import { By, until, Builder, Capabilities, Locator } from "selenium-webdriver"
+import { By, Builder, Capabilities, until, Locator } from 'selenium-webdriver';
+import { after, before } from 'mocha';
+import { expect } from 'chai';
 
-const driver = new Builder()
-    .withCapabilities(Capabilities.chrome())
-    .build();
-const baseUrl = "https://www.onliner.by/"
-const defaultWaitingTime = 3000;
-let pageUrl: string;
-let currentUrl: string;
+const baseUrl = 'https://www.onliner.by';
 
-describe("Onliner header menu tests", () => {
+describe('Onliner selenium-tests:', () => {
+     const driver = new Builder()
+         .withCapabilities(Capabilities.chrome())
+         .build();
 
     before(async () => {
         await driver.manage().window().maximize();
-    })
-
-    it("Should navigate to the catalog page from main page", async () => {
         await driver.get(baseUrl);
+    });
 
-        const catalotButtonLocator: Locator = By.css(`a[href = "https://catalog.onliner.by"] span.b-main-navigation__text`);
-        await driver.wait(until.elementsLocated(catalotButtonLocator), defaultWaitingTime);
-        await driver.findElement(catalotButtonLocator).click();
+    it('Should display page title is correctly', async () => {
+        await driver.wait(until.elementLocated(By.css('title')));
+        const textTitle = await driver.getTitle();
+        expect(textTitle).to.eq('Onliner');
+    });
 
-        pageUrl = "https://catalog.onliner.by/";
-        await driver.wait(until.urlIs(pageUrl), defaultWaitingTime);
-        const catalogTitle = await driver.findElement(By.css(`a.catalog-navigation__bubble`))
-        expect(await catalogTitle.getText()).to.be.deep.equal("Все суперцены!")
-    })
+    it('When User click "Каталог", the page is displayed with title including "Каталог"', async () => {
+        await driver.findElement(By.linkText('Каталог')).click();
+        const catalog = await driver.getTitle();
+        expect(catalog).to.include('Каталог');
+    });
 
-     it("Should navigate to the baraholka page from main page", async () => {
-         
+    it("Should navigate to the baraholka page from main page", async () => {
         await driver.get(baseUrl);
-   
         const baraholkaButtonLocator: Locator = By.css(`a[href="https://baraholka.onliner.by/"] span.b-main-navigation__text`)
-        await driver.wait(until.elementsLocated(baraholkaButtonLocator), defaultWaitingTime);
+        await driver.wait(until.elementsLocated(baraholkaButtonLocator), 3000);
         await driver.findElement(baraholkaButtonLocator).click();
-   
-        pageUrl = "https://baraholka.onliner.by/";
-        await driver.wait(until.urlIs(pageUrl), defaultWaitingTime);
+        const pageUrl = "https://baraholka.onliner.by/";
+        await driver.wait(until.urlIs(pageUrl), 3000);
         const baraholkaPageTitle = await driver.findElement(By.xpath(`//div[contains(@class, "b-mnforum-header-i")] //h1`))
         expect(await baraholkaPageTitle.getText()).to.be.deep.equal("Барахолка")
     })
 
-        after(async () => {
+
+
+after(async () => {
         await driver.quit();
-    })
-}) 
-
-
-
+    });
+});
