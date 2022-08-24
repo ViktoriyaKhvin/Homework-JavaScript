c2b6694b1edeef2b3b25f8cacf05a3224ce49746
ViktoriyaKhvin <khvinka@yandex.by>
2022-08-24 12:15:01 +0300

    hw19

--- hw19/selenium-test/onliner.spec.ts
+++ hw19/selenium-test/onliner.spec.ts
@@ -1,29 +1,29 @@
-import { Builder, Capabilities, By, until, WebElement, } from 'selenium-webdriver';
+import { By, Builder, Capabilities, until } from 'selenium-webdriver';
+import { after, before } from 'mocha';
 import { expect } from 'chai';
 
- import { By, Builder, Capabilities, until } from 'selenium-webdriver';
- import { after, before } from 'mocha';
- import { expect } from 'chai';
+const baseUrl = 'https://www.onliner.by';
 
- const baseUrl = 'https://www.onliner.by';
- const driver = new Builder().withCapabilities(Capabilities.chrome()).build();
+describe('Onliner selenium-tests'), () => {
+     const driver = new Builder()
+         .withCapabilities(Capabilities.chrome())
+         .build();
+     before(async () => {
+         await driver.manage().window().maximize();
+         await driver.get(baseUrl);
+     });
 
- before(async () => {
-    await driver.manage().window().maximize();
-    await driver.get(baseUrl);
-});
+it('Should display page title is correctly', async () => {
+         await driver.wait(until.elementLocated(By.css('title')));
+         const textTitle = await driver.getTitle();
+         expect(textTitle).to.eq('Onliner');
+     })};
 
-it('Should display the title of main page correctly', async () => {
-    await driver.get(baseUrl);
-    const currentTitle = await driver.getTitle();
-    expect(currentTitle).to.equal('Onliner');
-});
 
-it('When User click "Форум", the page is displayed with title including "Форум"', async () => {
-    await driver.findElement(By.linkText('Форум')).click();
-    const catalog = await driver.getTitle();
-    expect(catalog).to.include('Форум');
-});
-  
+
+
+
+
+
 
 
