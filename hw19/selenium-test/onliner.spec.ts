7b219c983381cf4f29d4ebe47f4bbe4c3afc421c
ViktoriyaKhvin <khvinka@yandex.by>
2022-08-28 21:58:24 +0300

    hw19

--- hw19/selenium-test/onliner.spec.ts
+++ hw19/selenium-test/onliner.spec.ts
@@ -19,32 +19,18 @@ describe("Onliner selenium-tests", () => {
     })
 
 it("The catalog page should open from the home page", async () => {
-
         const catalotButtonLocator: Locator = By.css(`a[href = "https://catalog.onliner.by"] span.b-main-navigation__text`);
         await driver.wait(until.elementsLocated(catalotButtonLocator), defaultWaitingTime);
         await driver.findElement(catalotButtonLocator).click();
-
         pageUrl = "https://catalog.onliner.by/";
         await driver.wait(until.urlIs(pageUrl), defaultWaitingTime);
         const catalogTitle = await driver.findElement(By.css(`a.catalog-navigation__bubble`))
         expect(await catalogTitle.getText()).to.be.deep.equal("Все суперцены!")
         })
 
-it("Should navigate to the realt page from main page", async () => {
-        const realtButtonLocator: Locator = By.css(`a[href = "https://r.onliner.by/pk"] span.b-main-navigation__text`)
-        await driver.wait(until.elementsLocated(realtButtonLocator), defaultWaitingTime);
-        await driver.findElement(realtButtonLocator).click();
-   
-        pageUrl = "https://r.onliner.by/pk/";
-        await driver.wait(until.urlIs(pageUrl), defaultWaitingTime);
-        const sellButton = await driver.findElement(By.css(`a[href="https://r.onliner.by/pk/"] span.project-navigation__sign`))
-        expect(await sellButton.getText()).to.be.deep.equal("Продажа")
-        })
-   
 it("Should navigate to the tasks page from main page", async () => {
         const tasksButtonLocator: Locator = By.xpath(`//a[@href = "https://s.onliner.by/tasks"]/span[contains(@class, "b-main-navigation__text")]`)
         await driver.wait(until.elementsLocated(tasksButtonLocator), defaultWaitingTime);
-   
         await driver.findElement(tasksButtonLocator).click();
         pageUrl = "https://s.onliner.by/tasks";
         await driver.wait(until.urlIs(pageUrl), defaultWaitingTime);
@@ -52,11 +38,20 @@ it("Should navigate to the tasks page from main page", async () => {
         expect(await tasksPageTitle.getText()).to.be.deep.equal("Заказы")
         })
 
+it("Should navigate to the realt page from main page", async () => {
+        const realtButtonLocator: Locator = By.css(`a[href = "https://r.onliner.by/pk"] span.b-main-navigation__text`)
+        await driver.wait(until.elementsLocated(realtButtonLocator), defaultWaitingTime);
+        await driver.findElement(realtButtonLocator).click();
+        pageUrl = "https://r.onliner.by/pk/";
+        await driver.wait(until.urlIs(pageUrl), defaultWaitingTime);
+        const sellButton = await driver.findElement(By.css(`a[href="https://r.onliner.by/pk/"] span.project-navigation__sign`))
+        expect(await sellButton.getText()).to.be.deep.equal("Продажа")
+        })
+   
 it("Should navigate to the baraholka page from main page", async () => {
         const baraholkaButtonLocator: Locator = By.css(`a[href="https://baraholka.onliner.by/"] span.b-main-navigation__text`)
         await driver.wait(until.elementsLocated(baraholkaButtonLocator), defaultWaitingTime);
         await driver.findElement(baraholkaButtonLocator).click();
-   
         pageUrl = "https://baraholka.onliner.by/";
         await driver.wait(until.urlIs(pageUrl), defaultWaitingTime);
         const baraholkaPageTitle = await driver.findElement(By.xpath(`//div[contains(@class, "b-mnforum-header-i")] //h1`))
@@ -67,16 +62,13 @@ it("Should show protection pop up while login with correct credentials", async (
         const loginButtonLocator: Locator = By.className("auth-bar__item auth-bar__item--text")
         await driver.wait(until.elementsLocated(loginButtonLocator), defaultWaitingTime)
         await driver.findElement(loginButtonLocator).click();
-   
         const emailFieldLocator: Locator = By.css("div.auth-input__wrapper input[type = text]")
         await driver.wait(until.elementsLocated(emailFieldLocator), defaultWaitingTime)
         await driver.findElement(emailFieldLocator).click();
         await driver.findElement(emailFieldLocator).sendKeys("test12051@mail.ru");
-   
         const passwordFieldLocator: Locator = By.css("div.auth-input__wrapper input[type = password]")
         await driver.findElement(passwordFieldLocator).click();
         await driver.findElement(passwordFieldLocator).sendKeys("test12051");
-   
         const submitButton = await driver.findElement(By.css("div.auth-form__control button[type = submit]"));
         await submitButton.click();
         const protectionPopUpLocator: Locator = By.className("auth-form__mediabox");
@@ -84,6 +76,7 @@ it("Should show protection pop up while login with correct credentials", async (
         const element = await driver.findElement(By.css("div.auth-form__mediabox span"));
         expect(await element.getText()).to.have.string(`Давайте проверим, вы робот или нет`);
         })
+        
 after(async () => {
         await driver.quit();
         })
