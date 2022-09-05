e33b32e795b28194f72d8cf77ca0b48713fc38f2
ViktoriyaKhvin <khvinka@yandex.by>
2022-09-05 15:03:58 +0300

    hw19

--- hw19/selenium-test/onliner.spec.ts
+++ hw19/selenium-test/onliner.spec.ts
@@ -1,86 +1,101 @@
-import { expect } from 'chai';
-import { mkdirSync, rmSync, writeFile } from 'fs';
-import { Builder, Capabilities, By, until } from 'selenium-webdriver';
-import { Context } from 'mocha';
+ import { expect } from "chai";
+ import { By, until, Builder, Capabilities, Locator } from "selenium-webdriver"
 
-const driver = new Builder().withCapabilities(Capabilities.chrome()).build();
-const baseUrl = 'https://www.onliner.by/';
-const titleOfMainPage = 'Onliner';
-const textForMotors = 'Моторные масла';
-const catalogIncludingText = 'Каталог';
-const searchPlaceholderText = 'Поиск в Каталоге.';
+ const driver = new Builder()
+     .withCapabilities(Capabilities.chrome())
+     .build();
+ const baseUrl = "https://www.onliner.by/"
+ const defaultWaitingTime = 5000;
+ let pageUrl: string;
 
-const screensDir = 'HW_19/selenium/screenshots';
-let testsCounter = 1;
-const defaultWaitingTime = 3000;
+ describe("Onliner header menu tests", () => {
 
-describe('Tests of the site Onliner', function () {
-     before(function () {
-        rmSync(screensDir, { recursive: true, force: true });
-        mkdirSync(screensDir, { recursive: true });
-     });
+     before(async () => {
+         await driver.manage().window().maximize();
+     })
 
-     describe('Tests of the main page', async function () {
-        it('Should display the title of main page correctly', async function () {
-             await driver.manage().window().maximize();
-             await driver.get(baseUrl);
-             expect(await driver.getTitle()).to.equal(titleOfMainPage);
-        });
+     beforeEach(async () => {
+         await driver.get(baseUrl);
+     })
 
-        it(`When User clicks link ${textForMotors}, correct page is displayed with the title of article ${textForMotors}`, async function () {
-             await driver.findElement(By.linkText(textForMotors)).click();
-             const pageHeader = await driver.findElement(
-                By.className('schema-header__title js-schema-header_title')
-                );
-             expect(await pageHeader.getText()).to.equal(textForMotors);
-             await driver.executeScript(
-                 "arguments[0].setAttribute('style', 'background: #91c3f2;')",
-                 pageHeader
-             );
-        });
+     it("Should navigate to the catalog page from main page", async () => {
 
-        it('When User clicks on the logo of site, the main page is displayed', async function () {
-             await driver.findElement(By.className('b-top-logo')).click();
-             await driver.wait(until.urlContains(baseUrl), defaultWaitingTime);
-        });
+         const catalotButtonLocator: Locator = By.css(`a[href = "https://catalog.onliner.by"] span.b-main-navigation__text`);
+         await driver.wait(until.elementsLocated(catalotButtonLocator), defaultWaitingTime);
+         await driver.findElement(catalotButtonLocator).click();
 
-        it(`When User clicks ${catalogIncludingText}, the page is displayed with title including ${catalogIncludingText}`, async function () {
-             await driver.findElement(By.linkText(catalogIncludingText)).click();
-             expect(await driver.getTitle()).to.include(catalogIncludingText);
-        });
+         pageUrl = "https://catalog.onliner.by/";
+         await driver.wait(until.urlIs(pageUrl), defaultWaitingTime);
+         const catalogTitle = await driver.findElement(By.css(`a.catalog-navigation__bubble`))
+         expect(await catalogTitle.getText()).to.be.deep.equal("Все суперцены!")
+        })
 
-        it(`The main page contains search form with with placeholder including text ${searchPlaceholderText}`, async function () {
-             await driver.findElement(By.className('b-top-logo')).click();
-             const searchElement = await driver.findElement(
-                By.xpath(
-                     '//form[@class="fast-search__form"]/input[@type="text"]'
-                )
-             );
-             expect(await searchElement.getAttribute('placeholder')).to.include(
-                searchPlaceholderText
-             );
-             await driver.executeScript(
-                "arguments[0].setAttribute('style', 'background: #91c3f2;')",
-                searchElement
-            );
-        });
+        it("Should navigate to the Auto shope page from main page", async () => {
+            const autoShopButtonLocator: Locator = By.xpath(`//a[@href = "https://ab.onliner.by"]/span[contains(@class, "b-main-navigation__text")]`);
+            await driver.wait(until.elementsLocated(autoShopButtonLocator), defaultWaitingTime);
+            await driver.findElement(autoShopButtonLocator).click();
+   
+            pageUrl = "https://ab.onliner.by/";
+            await driver.wait(until.urlIs(pageUrl), defaultWaitingTime);
+            const autoPageTitle = await driver.findElement(By.css("div.vehicle-form h1"))
+            expect(await autoPageTitle.getText()).to.be.deep.equal("Автобарахолка")
+        })
+   
+        it("Should navigate to the realt page from main page", async () => {
+            const realtButtonLocator: Locator = By.css(`a[href = "https://r.onliner.by/pk"] span.b-main-navigation__text`)
+            await driver.wait(until.elementsLocated(realtButtonLocator), defaultWaitingTime);
+            await driver.findElement(realtButtonLocator).click();
+   
+            pageUrl = "https://r.onliner.by/pk/";
+            await driver.wait(until.urlIs(pageUrl), defaultWaitingTime);
+            const sellButton = await driver.findElement(By.css(`a[href="https://r.onliner.by/pk/"] span.project-navigation__sign`))
+            expect(await sellButton.getText()).to.be.deep.equal("Продажа")
+        })
+   
+        it("Should navigate to the tasks page from main page", async () => {
+            const tasksButtonLocator: Locator = By.xpath(`//a[@href = "https://s.onliner.by/tasks"]/span[contains(@class, "b-main-navigation__text")]`)
+            await driver.wait(until.elementsLocated(tasksButtonLocator), defaultWaitingTime);
+   
+            await driver.findElement(tasksButtonLocator).click();
+            pageUrl = "https://s.onliner.by/tasks";
+            await driver.wait(until.urlIs(pageUrl), defaultWaitingTime);
+            const tasksPageTitle = await driver.findElement(By.css(`a[href= "/tasks"] span.project-navigation__sign`))
+            expect(await tasksPageTitle.getText()).to.be.deep.equal("Заказы")
+        })
 
-        afterEach(async function () {
-            const data = await driver.takeScreenshot();
-            const base64Data = data.replace(/^data:image\/png;base64,/, '');
-            writeFile(
-                `${screensDir}/${testsCounter++}.${
-                    (this as Context).currentTest?.title
-                }.png`,
-                base64Data,
-                'base64',
-                function (err) {
-                    if (err) console.log(err.message);
-                }
-            );
-        });
-    });
-    after(async () => {
-        await driver.quit();
-    });
-});
+        it("Should navigate to the baraholka page from main page", async () => {
+            const baraholkaButtonLocator: Locator = By.css(`a[href="https://baraholka.onliner.by/"] span.b-main-navigation__text`)
+            await driver.wait(until.elementsLocated(baraholkaButtonLocator), defaultWaitingTime);
+            await driver.findElement(baraholkaButtonLocator).click();
+   
+            pageUrl = "https://baraholka.onliner.by/";
+            await driver.wait(until.urlIs(pageUrl), defaultWaitingTime);
+            const baraholkaPageTitle = await driver.findElement(By.xpath(`//div[contains(@class, "b-mnforum-header-i")] //h1`))
+            expect(await baraholkaPageTitle.getText()).to.be.deep.equal("Барахолка")
+        })
+
+        it("Should show protection pop up while login with correct credentials", async () => {
+            const loginButtonLocator: Locator = By.className("auth-bar__item auth-bar__item--text")
+            await driver.wait(until.elementsLocated(loginButtonLocator), defaultWaitingTime)
+            await driver.findElement(loginButtonLocator).click();
+   
+            const emailFieldLocator: Locator = By.css("div.auth-input__wrapper input[type = text]")
+            await driver.wait(until.elementsLocated(emailFieldLocator), defaultWaitingTime)
+            await driver.findElement(emailFieldLocator).click();
+            await driver.findElement(emailFieldLocator).sendKeys("test12051@mail.ru");
+   
+            const passwordFieldLocator: Locator = By.css("div.auth-input__wrapper input[type = password]")
+            await driver.findElement(passwordFieldLocator).click();
+            await driver.findElement(passwordFieldLocator).sendKeys("test12051");
+   
+            const submitButton = await driver.findElement(By.css("div.auth-form__control button[type = submit]"));
+            await submitButton.click();
+            const protectionPopUpLocator: Locator = By.className("auth-form__mediabox");
+            await driver.wait(until.elementsLocated(protectionPopUpLocator), defaultWaitingTime);
+            const element = await driver.findElement(By.css("div.auth-form__mediabox span"));
+            expect(await element.getText()).to.have.string(`Давайте проверим, вы робот или нет`);
+        })
+        after(async () => {
+                await driver.quit();
+            })
+        })
